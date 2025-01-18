a3c1fded69c692b125c2003d96eb1a7fb35bc7eb
lukhed <lukhed.mail@gmail.com>
2025-01-18 16:16:46 -0500

    final structure

--- lukhed_sports/dkWrapper.py
+++ lukhed_sports/dkWrapper.py
@@ -6,30 +6,26 @@ from lukhed_basic_utils import stringCommon as sC
 from lukhed_basic_utils import listWorkCommon as lC
 
 class DkSportsbook():
-    def __init__(self, sport=None, api_delay=0.3):
+    def __init__(self, api_delay=0.75):
         # Set API Information
         self.api_delay = api_delay
 
-        'https://sportsbook-nash.draftkings.com/api/sportscontent/dkusmi/v1/leagues/42133'
-        'https://sportsbook-nash.draftkings.com/api/sportscontent/navigation/dkusmi/v2/nav/sports?format=json'
-        
         # Set cals
         self._api_versions = None
         self._base_url = None
         self.sportsbook = None
         self._load_calibrations()
         
-        # Available data
+        # Available Sports
         self.available_sports = None
         self._available_sports = None
+        self._check_set_available_sports()
 
-        self.sport = sport.lower() if type(sport) is str else None
-        self._sport_id = self._get_sport_id(self.sport) if sport is not None else None
+        # Cache
+        self._cached_available_leagues_json = {}        # available leagues for a given sport cache
+        self._cached_league_json = {}                   # league json for a given league
+        self._cached_category = None
 
-        self.available_leagues = None
-        self._available_leagues = None
-        if self._sport_id is not None:
-            self._set_available_leagues()
 
     def _load_calibrations(self):
         # Load version cal
@@ -52,114 +48,257 @@ class DkSportsbook():
         print("called api")
         response = rC.request_json(endpoint, add_user_agent=True)
         return response
-
-    def _get_json_for_league(self):
-        add_url = f'leagues/{self._league_id}'
-        url = self._base_url + add_url
-        response = self._call_api(url)
-
-        return response
     
-    def _get_category_id(self, category):
-        category = category.lower()
-        for cat in self._league_json['categories']:
-            if cat['name'].lower() == category:
-                return cat['id']
+    ############################
+    # Class cache management
+    ############################
+    def _check_available_league_cache(self, sport_id):
+        """
+        This function tries to utilize saved available leagues for a sport. Useful for users doing multiple 
+        queries against the same sport so as to save api calls.
 
-        print(f"""ERROR: _get_category_id() could not match category Id for {self._league_id}, 
-              {category}. You can raise a bug below by copy and pasting this error message.\n\n
-                  https://github.com/lukhed/lukhed_sports/issues""")
+
+        Parameters
+        ----------
+        sport_id : str()
+            League being requested
+
+        Returns
+        -------
+        dict()
+            Output from self._get_json_for_league() or blank dict {} if no cache
+        """
+
+        try:
+            available_leagues_json = self._cached_available_leagues_json[sport_id]
+        except KeyError:
+            available_leagues_json = None
         
-        return None
+        return available_leagues_json
     
+    def _get_league_data_for_sport(self, s_id):
+        # check cache for id, get from dk if not in cache
+        available_leagues_cache = self._check_available_league_cache(s_id)
+        if available_leagues_cache is not None:
+            # cache is available
+            print("Used available league json cache")
+            return available_leagues_cache
+        else:
+            # obtain league json from dk and add to cache
+            api_version = self._api_versions['navVersion']
+            url = f"{self._base_url}/sportscontent/navigation/{self.sportsbook}/{api_version}/nav/sports/{s_id}?format=json"
+            available_leagues = self._call_api(url)
+            self._cached_available_leagues_json[s_id] = available_leagues
+            return available_leagues
+    
+    def _check_league_json_cache(self, league_id):
+        """
+        This function tries to utilize saved league json if available. Useful for users doing multiple queries against 
+        the same leagues so as to save api calls.
+
+        When a user makes a call for data from dk with a league input parameter, the self._get_json_for_league() 
+        function checks the cache and sets the cache when applicable.
+
+        Parameters
+        ----------
+        league_json : str()
+            League being requested
+
+        Returns
+        -------
+        dict()
+            Output from self._get_league_data_for_sport() or blank dict {} if no cache
+        """
+
+        try:
+            league_json = self._cached_league_json[league_id]
+        except KeyError:
+            league_json = None
+        
+        return league_json
+    
+    def _get_json_for_league(self, sport, league):
+        sport = sport.lower()
+        league = league.lower()
+        'https://sportsbook-nash.draftkings.com/api/sportscontent/dkusmi/v1/leagues/88808'
+
+        league_id = self._get_league_id(sport, league)
+
+        # check cache for id, get from dk if not in cache
+        league_json_cache = self._check_league_json_cache(league_id)
+        if league_json_cache is not None:
+            # cache is available
+            print("Used league json cache")
+            return league_json_cache
+        else:
+            # obtain league json from dk and add to cache
+            api_version = self._api_versions['groupVersion']
+            url = f"{self._base_url}/sportscontent/{self.sportsbook}/{api_version}/leagues/{league_id}"
+            league_json = self._call_api(url)
+            self._cached_league_json[league_id] = league_json
+
+        return league_json
+    
+    def _get_sport_from_id(self, sport_id):
+        sport_ids = [x['id'] for x in self._available_sports]
+        sport = self.available_sports[sport_ids.index(sport_id)]
+        return sport
+    
+    ############################
+    # Input Checks
+    ############################
+    def _check_valid_sport(self, sport):
+        sport = sport.lower()
+        if sport in self.available_sports:
+            return True
+        else:
+            print(f"ERROR: '{sport}' is not a valid sport. Check api.available_sports for valid input.")
+            return False
+
+    ############################
+    # API Parsing
+    ############################   
     def _get_sport_id(self, sport):
+        """
+        Gets the sport ID for a given sport by parsing the self._available_sports variable which is set upon 
+        class instantiation.
+
+        sport
+          -> league
+            -> category
+            -> events
+            -> markets
+
+        Parameters
+        ----------
+        league : str()
+            A sport string available from dk
+
+        Returns
+        -------
+        str()
+            A sport ID which is used in API calls.
+        """
         sport = sport.lower()
         self._check_set_available_sports()
         if sport in self.available_sports:
             index = self.available_sports.index(sport)
             return self._available_sports[index]['id']
         else:
-            print(f"ERROR: Could not find {sport} in valid sports. Try 'get_available_sports()' to see valid sports")
+            print(f"ERROR: Could not find {sport} in valid sports. Check api.available_sports for valid input.")
             return None
-
-    def _get_league_data_for_sport(self, s_id):
-        api_version = self._api_versions['navVersion']
-        url = f"{self._base_url}/sportscontent/navigation/{self.sportsbook}/{api_version}/nav/sports/{s_id}?format=json"
-        league_data = self._call_api(url)
-        return league_data
-
-    def _set_available_leagues(self):
-        league_data = self._get_league_data_for_sport(self._sport_id)
-        self.available_leagues = [x['name'] for x in league_data['leagues']]
-        self._available_leagues = league_data['leagues']
         
-    def set_sport(self, sport):
-        self._sport_id = self._get_sport_id(sport)
-        if self._sport_id is None:
-            self.sport = None
-        else:
-            self.sport = sport.lower()
-            self._set_available_leagues()
+    def _get_league_id(self, sport, league):
+        """
+        Gets the league ID for a given league by checking cache or collecting league id from dk.
+
+        sport
+          -> league
+            -> category
+
+        Parameters
+        ----------
+        league : str()
+            A league string available for the given sport
+
+        Returns
+        -------
+        str()
+            A league ID corresponding to the given league which is used in API calls.
+        """
+
+
+        sport_id = self._get_sport_id(sport)
+        available_leagues = self._get_league_data_for_sport(sport_id)
+        league = league.lower()
+
+        for available_league in available_leagues['leagues']:
+            league_name = available_league['name'].lower()
+            if league_name == league:
+                return available_league['id']
             
-    def get_available_sports(self):
-        self._check_set_available_sports()
-        return self.available_sports
+        print(f"ERROR: '{league}' was not found for '{sport}'. Use api.get_available_leagues() to check valid input")
+        return None
     
-    def get_available_leagues_for_sport(self, sport):
-        """
-        Provide a valid sport to check the available leagues available for lines. Use get_available_sports() to 
-        get a list of valid sports.
-
-        Parameters
-        ----------
-        sport : str()
-            A valid draftkings sport
-        """
-
-        'https://sportsbook-nash.draftkings.com/api/sportscontent/navigation/dkusmi/v2/nav/sports?format=json'
-
+    def get_available_leagues(self, sport):
+        sport_id = self._get_sport_id(sport)
+        league_data = self._get_league_data_for_sport(sport_id)
+        return [x['name'].lower() for x in league_data['leagues']]
+    
+    def _get_data_from_league_json(self, sport, league, key):
         sport = sport.lower()
-        self._check_set_available_sports()
-        if sport in self.available_sports:
-            s_id = self._get_sport_id(sport)
-            league_data = self._get_league_data_for_sport(s_id)
-            return [x['name'] for x in league_data['leagues']]
-        else:
-            print(f"ERROR: Could not find {sport} in valid sports. Try 'get_available_sports()' to see valid sports")
+        league = league.lower()
+        key = key.lower()
+
+        league_json = self._get_json_for_league(sport, league)
+
+        # test the key availability
+        try:
+            league_json[key]
+            data = [x['name'].lower() for x in league_json[key]]
+        except KeyError:
+            data = []
+            
+        if len(data) == 0:
+            print(f"INFO: There are no '{key}' for {sport} - {league}.")
+
+        return data
+    
+
+    ############################
+    # Core User Facing
+    ############################
+    def get_available_leagues(self, sport):
+        sport_id = self._get_sport_id(sport)
+        league_data = self._get_league_data_for_sport(sport_id)
+        return [x['name'].lower() for x in league_data['leagues']]
+
+    def get_available_betting_categories(self, sport, league):
+        if not self._check_valid_sport(sport):
             return []
+
+        categories = self._get_data_from_league_json(sport, league, 'categories')
+        return categories
     
-    def get_available_betting_categories(self):
-        return self.available_betting_categories
-
-    def get_market_by_category(self, category):
-        if category.lower() not in self.available_betting_categories:
-            print(f"""ERROR: No betting category {category} available. Use function 
-                  get_available_betting_categories() to get a list of available categories. If you are using a 
-                  supported category and still getting this error, raise an issue below and paste this error 
-                  message.\n\n
-                  https://github.com/lukhed/lukhed_sports/issues""")
-            return {}
-
-        cat_id = self._get_category_id(category)
-        url = f'{self._base_url}leagues/{self._league_id}/categories/{cat_id}'
-        response = rC.request_json(url, add_user_agent=True)
-
-        return response['selections']
+    def get_available_betting_events(self, sport, league):
+        if not self._check_valid_sport(sport):
+            return []
+        
+        events = self._get_data_from_league_json(sport, league, 'events')
+        return events
     
-    def set_league(self, league):
-        self.league = league.lower()
-        self._set_league_data()
+    def get_available_betting_markets(self, sport, league):
+        if not self._check_valid_sport(sport):
+            return []
+        markets = self._get_data_from_league_json(sport, league, 'markets')
+        return lC.return_unique_values(markets)
 
-    def get_available_leagues(self, match_fuzzy_search_term=None):
-        """
-        Returns a list of all available leagues. Optionally complete a fuzzy search to 
-        narrow down results.
+    def get_betting_selections_by_category(self, sport, league, category):
+        category = category.lower()
+        league_json = self._get_json_for_league(sport, league)
+        available = self.get_available_betting_categories(sport, league)
+        if category not in available:
+            print(f"""ERROR: '{category}' is not available for '{league}'. Use function 
+                  get_available_betting_categories() to get valid input.""")
+            return []
+        
+        market_index = available.index(category)
+        league_id = self._get_league_id(sport, league)
+        cat_id = league_json['categories'][market_index]['id']
+        api_version = self._api_versions['groupVersion']
+        url = f"{self._base_url}/sportscontent/{self.sportsbook}/{api_version}/leagues/{league_id}/categories/{cat_id}"
+        selections = self._call_api(url)['selections']
+        
+        return selections
+    
+    def get_event_data(self, sport, league, event):
+        event = event.lower()
+        league_json = self._get_json_for_league(sport, league)
+        events = league_json['events']
+        for available in events:
+            if available['name'].lower() == event:
+                return available
 
-        Parameters
-        ----------
-        match_fuzzy_search_term : str(), optional
-            The term you want to use to search all available leagues. Only leagues that are 
-            close to the string provided will be returned. By default None
-        """
-
-        stop = 1
+        print(f"""ERROR: '{event}' is not available for '{league}'. Use function 
+                  get_available_betting_events() to get valid input.""")
+        return {}
