b5d421c2e931f7889dae5664f43e18396d5ba990
lukhed <lukhed.mail@gmail.com>
2025-06-08 18:00:58 -0400

    working methods

--- lukhed_sports/nflSchedules.py
+++ lukhed_sports/nflSchedules.py
@@ -6,40 +6,26 @@ from lukhed_basic_utils import requestsCommon as rC
 
 class NextGenStatsSchedule:
 
-    def __init__(self, season):
+    def __init__(self, season='current'):
         """
         This class utilizes Next Gen Stats public APIs to get the NFL schedule.
 
         season: str, optional
-            The season for which to retrieve the schedule.
+            The season for which to retrieve the schedule. By default, it is set to 'current'.
+
+            In the post and off-season, the 'current' schedule may not be what you expect, and you may have to 
+            input the season in the season parameter.
         """
         self.ngs_header = {'Referer': 'https://nextgenstats.nfl.com/stats/game-center/2023100200'}
 
-        self.ngs_current_data = {}
         self.ngs_schedule_data = {}
-        self.ngs_regular_season_schedule = {}
         self.ngs_game_ids = {}
-        self.season = int(season)
+        self.season = "current" if type(season) == str and season.lower() == 'current' else int(season)
 
-        self._check_get_ngs_schedule_data(force_overwrite=True)
+        self._check_get_ngs_schedule_data()
 
-    def _check_set_season_and_week(self, season_input):
-        if self.season is None:
-            self._check_get_ngs_current_file()
 
-        self.season = int(season_input) if season_input != 'current' else self.ngs_current_data['season']
-        self.week = self.ngs_current_data['week']
-
-    def _check_get_ngs_current_file(self):
-        """
-        Look in the console, this document is labeled 'current'
-        :return:
-        """
-        if self.ngs_current_data == {}:
-            url = 'https://nextgenstats.nfl.com/api/league/schedule/current'
-            self.ngs_current_data = self._call_api(url)
-
-    def _check_get_ngs_schedule_data(self, force_overwrite=False):
+    def _check_get_ngs_schedule_data(self, force_season_overwrite=None):
         """
         Gets the scheduled based on the current season. Param force_overwrite is used by change_season function.
 
@@ -48,49 +34,136 @@ class NextGenStatsSchedule:
         :return:
         """
 
-        if self.ngs_schedule_data == {} or force_overwrite:
-            url = f'https://nextgenstats.nfl.com/api/league/schedule?season={self.season}'
+        self.season = force_season_overwrite if force_season_overwrite is not None else self.season
+
+        if self.ngs_schedule_data == {} or force_season_overwrite:
+            if self.season == 'current':
+                url = 'https://nextgenstats.nfl.com/api/league/schedule/current'
+            else:
+                url = f'https://nextgenstats.nfl.com/api/league/schedule?season={self.season}'
             self.ngs_schedule_data = self._call_api(url)
-            self.ngs_regular_season_schedule = [x for x in self.ngs_schedule_data if x['seasonType'] == 'REG']
+
+            if type(self.ngs_schedule_data) == list:
+                pass
+            else:
+                try:
+                    # If current schedule is post season data, it will be in a different format
+                    self.ngs_schedule_data = self.ngs_schedule_data['games']
+                except KeyError:
+                    pass
 
     def _call_api(self, url):
         return rC.request_json(url, add_user_agent=True, headers=self.ngs_header)
+    
+    def _get_game_id(self, team, week):
+        """
+        Gets the game ID for a specific team and week from the Next Gen Stats schedule.
 
-    def change_season(self, season):
-        self._check_set_season_and_week(season)
-        self._check_get_ngs_schedule_data(force_overwrite=True)
+        Parameters
+        ----------
+        team : str
+            The abbreviation of the team for which to retrieve the game ID (e.g., 'DET' for Detroit Lions).
+            You can check the appropriate team abbreviations by visting NGS game center page:
+            https://nextgenstats.nfl.com/stats/game-center-index
 
-    def get_game_data(self, team, week):
-        self._check_get_ngs_schedule_data()
-        team = team.lower()
-        team_data = [x for x in self.ngs_regular_season_schedule if
-                     (x['visitorTeamAbbr'].lower() == team or x['homeTeamAbbr'].lower() == team) and
-                     x['week'] == int(week)][0]
+        week : str or int
+            For regular season, this should be an integer from 1 to 18.
+            For post-season, this can be 'WC' for wildcard, 'DIV' for division, 'CONF' for conference, or 'SB' 
+            for Super Bowl.
 
-
-        return team_data
-
-    def get_game_id(self, team, week):
+        Returns
+        -------
+        str
+            The game ID for the specified team and week. This ID can be used to retrieve more detailed game data
+        """
         return self.get_game_data(team, week)['gameId']
-
-    def get_team_id(self, team):
+    
+    def _get_team_id(self, team):
         self._check_get_ngs_schedule_data()
         team = team.lower()
         team_id = None
-        for game in self.ngs_regular_season_schedule:
+        for game in self.ngs_schedule_data:
             if game['visitorTeamAbbr'].lower() == team or game['homeTeamAbbr'].lower() == team:
                 team_id = game['homeTeamId'] if game['homeTeamAbbr'].lower() == team else game['visitorTeamId']
                 break
 
         return team_id
 
+    def change_season(self, season):
+        """
+        Changes the season for which the Next Gen Stats schedule is retrieved.
+
+        Parameters
+        ----------
+        season : str or int
+            The season for which to retrieve the schedule. It can be 'current' or an integer representing a 
+            specific season.
+        """
+        self._check_get_ngs_schedule_data(force_overwrite=season)
+
+    def get_regular_season_games(self):
+        """
+        Returns the regular season games from the Next Gen Stats schedule.
+
+        Returns
+        -------
+        list
+            A list of dictionaries containing the regular season games.
+        """
+        self._check_get_ngs_schedule_data()
+        return [x for x in self.ngs_schedule_data if x['seasonType'] == 'REG']
+    
+    def get_game_data(self, team, week):
+        """
+        Gets the game data for a specific team and week from the Next Gen Stats schedule.
+
+        Parameters
+        ----------
+        team : str
+            The abbreviation of the team for which to retrieve the game data (e.g., 'DET' for Detroit Lions).
+            You can check the appropriate team abbreviations by visting NGS game center page:
+            https://nextgenstats.nfl.com/stats/game-center-index
+
+        week : int or str:
+            For regular season, this should be an integer from 1 to 18.
+            For post-season, this can be 'WC' for wildcard, 'DIV' for division, 'CONF' for conference, or 'SB' 
+            for Super Bowl.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the game data for the specified team and week. The dictionary includes various
+        """
+        self._check_get_ngs_schedule_data()
+        team = team.lower()
+        team_data = [x for x in self.ngs_schedule_data if
+                     (x['visitorTeamAbbr'].lower() == team or x['homeTeamAbbr'].lower() == team) and
+                     x['week'] == int(week)][0]
+
+
+        return team_data
+
     def get_game_overview_for_team(self, team, week):
-        game_id = self.get_game_id(team, week)
+        game_id = self._get_game_id(team, week)
         url = f'https://nextgenstats.nfl.com/api/gamecenter/overview?gameId={game_id}'
         game_overview = self._call_api(url)
         return game_overview
 
-    def get_schedule(self, force_overwrite=False):
-        self._check_get_ngs_schedule_data(force_overwrite=force_overwrite)
+    def get_schedule(self, force_season_overwrite=None):
+        """
+        Gets the NFL schedule data from Next Gen Stats.
 
-       
+        Parameters
+        ----------
+        force_season_overwrite : str or int, optional
+            If provided, forces the retrieval of the schedule for the specified season, overriding the current season.
+            If None, uses the current season set in the instance.
+
+        Returns
+        -------
+        list
+        A list of dictionaries containing the NFL schedule data for the specified season.
+        """
+        self._check_get_ngs_schedule_data(force_season_overwrite=force_season_overwrite)
+        return self.ngs_schedule_data
+     
